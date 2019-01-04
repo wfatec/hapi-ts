aedbf31f454d952071367f828653bcceda3273af
wfatec <chao125010@163.com>
2019-01-04 14:28:45 +0800

    connect postgres with typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,78 @@
 'use strict'
 
 import * as hapi from 'hapi';
-// const hapi = require('hapi');
-// const server: hapi.Server = new hapi.Server()
-const server: hapi.Server = new hapi.Server({
+import "reflect-metadata";
+import {createConnection, Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { any } from 'joi';
+
+const server: any = new hapi.Server({
     port: 3000
 })
+
+@Entity("Entries")
+export class Entry {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    slug: string;
+
+    @Column({
+        nullable: true
+    })
+    content: string;
+}
 
 server.route({
     method: 'GET',
     path: '/',
-    handler: (request: hapi.Request, h: hapi.HandlerDecorationMethod) => {
+    handler: async (request: hapi.Request, h: hapi.HandlerDecorationMethod) => {
+        const entryRepo = server.app.dbConnection.getRepository(Entry);
+        const entries = await entryRepo.find();
+        return entries;
+    }
+});
 
-        return 'Hello, world!!';
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: async (request, h) => {
+
+        let {id, title, slug, content} = request.payload;
+
+        let newEntry = new Entry();
+        newEntry.id = id;
+        newEntry.title = title;
+        newEntry.slug = slug;
+        newEntry.content= content;
+
+        let entryRepo = server.app.dbConnection.getRepository(Entry);
+        await entryRepo.save(newEntry);
+
+        return "New post created"
     }
 });
 
 const init = async () => {
+    const connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "123456",
+        database: "test",
+        entities: [
+            Entry
+        ],
+        synchronize: true,
+        logging: false
+    });
     await server.start();
+    server.app.dbConnection = connection;
     console.log(`Server running at: ${server.info.uri}`)
 };
 
