48b5db5741144eccf169b2a63e3f29bd316f3f31
wfatec <chao125010@163.com>
2019-01-07 18:10:58 +0800

    use .env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,22 @@
 import {Entry} from '../models/entry';
 import {createConnection} from "typeorm";
 
-export default async () => {
+export default async ({
+    POSTGRES_PORT,
+    POSTGRES_HOST,
+    POSTGRES_DB_NAME,
+    POSTGRES_USERNAME,
+    POSTGRES_PASSWORD
+}) => {
     return await createConnection({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "postgres",
-        password: "123456",
-        database: "test",
+        host: POSTGRES_HOST||"localhost",
+        port: POSTGRES_PORT||5432,
+        username: POSTGRES_USERNAME||"postgres",
+        password: POSTGRES_PASSWORD||"123456",
+        database: POSTGRES_DB_NAME||"test",
         entities: [
             Entry
-        ],
-        synchronize: true,
-        logging: false
+        ]
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,40 @@
 
 import "reflect-metadata";
 import * as Glue from "glue";
+import * as dotenv from 'dotenv';
+import * as confidence from "confidence";
 import createDBConnection from './config/db';
 import manifest from './config/server';
+import { any } from "joi";
+
+// 设置环境变量
+dotenv.config();
+// const config = require("./config/config");
+// const store = new confidence.Store();
+// store.load(config)
+// const manifest = store.get("/", { env: process.env.NODE_ENV })
+// console.log('manifest: ',manifest)
+
+const {
+    POSTGRES_PORT,
+    POSTGRES_HOST,
+    POSTGRES_DB_NAME,
+    POSTGRES_USERNAME,
+    POSTGRES_PASSWORD
+} = process.env;
 
 const options = {
     relativeTo: __dirname
 };
 
 const init = async () => {
-    const connection = await createDBConnection();
+    const connection = await createDBConnection({
+        POSTGRES_PORT,
+        POSTGRES_HOST,
+        POSTGRES_DB_NAME,
+        POSTGRES_USERNAME,
+        POSTGRES_PASSWORD
+    });
 
     const server = await Glue.compose(manifest, options);
 
